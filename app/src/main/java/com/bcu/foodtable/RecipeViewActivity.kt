efe86851c1cd96242f66d8628702059cc999ff92
MintSoda07 <79971109+MintSoda07@users.noreply.github.com>
2025-01-23 01:10:08 +0900

    아이템 내부 UI 및 조리 기능 추가

--- app/src/main/java/com/bcu/foodtable/RecipeViewActivity.kt
+++ app/src/main/java/com/bcu/foodtable/RecipeViewActivity.kt
@@ -1,13 +1,18 @@
 package com.bcu.foodtable
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bcu.foodtable.useful.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +20,10 @@ import kotlinx.coroutines.launch
 
 class RecipeViewActivity : AppCompatActivity() {
     private lateinit var recipeId: String
-
+    private lateinit var adaptorViewList: RecyclerView
+    private lateinit var RecipeAdaptor: RecipeDetailRecyclerAdaptor
+    private lateinit var items: List<String>
+    private val regex = Regex("(.*)\\s*\\((.*),(\\d{2}:\\d{2}:\\d{2})\\)") // 타이머가 포함된 형식
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,6 +33,9 @@ class RecipeViewActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        adaptorViewList = findViewById(R.id.RecipeList)
+        adaptorViewList.layoutManager = LinearLayoutManager(this)
+
         // Intent로 전달된 데이터 받기
         recipeId = intent.getStringExtra("recipe_id") ?: ""
         CoroutineScope(Dispatchers.Main).launch {
@@ -36,11 +47,16 @@ class RecipeViewActivity : AppCompatActivity() {
                 val placeholder_name = findViewById<TextView>(R.id.itemName_recipe)
                 val placeholder_description = findViewById<TextView>(R.id.BasicDescription)
                 val placeholder_image = findViewById<ImageView>(R.id.itemImageView)
-                val inputString = "%1. 밥을 준비합니다. %.2. 밥을 삶습니다. %3. 밥을 먹습니다."
-
-                // %를 기준으로 문자열을 나눔 (
-                val items = inputString.split("%").filter { it.isNotBlank() }
-                FireStoreHelper.loadImageFromUrl(it.imageResId,placeholder_image)
+                val inputString = it.order
+                // ○를 기준으로 문자열을 나눔 (
+                items = inputString.split("○").filter { it.isNotBlank() }
+                // 리스트 어댑터
+                RecipeAdaptor = RecipeDetailRecyclerAdaptor(mutableListOf()) { position ->
+                    onDoneButtonClick(position)
+                }
+                RecipeAdaptor.updateItems(items)
+                adaptorViewList.adapter = RecipeAdaptor
+                FireStoreHelper.loadImageFromUrl(it.imageResId, placeholder_image)
                 placeholder_name.text = it.name
                 placeholder_description.text = it.description
 
@@ -50,4 +66,42 @@ class RecipeViewActivity : AppCompatActivity() {
         }
 
     }
+
+    // Done 버튼 클릭 시 처리할 로직
+    private fun onDoneButtonClick(position: Int) {
+        // 현재 항목을 숨기고 다음 항목을 보이게 하는 로직을 구현합니다.
+        val currentViewHolder =
+            adaptorViewList.findViewHolderForAdapterPosition(position) as RecipeDetailRecyclerAdaptor.ViewHolder?
+
+        // Done 버튼 숨기기
+        currentViewHolder?.doneButton?.visibility = View.GONE
+
+        // CheckBox 체크 상태 변경
+        currentViewHolder?.checkBox?.isChecked = true
+
+        // 배경색을 회색으로 흐리게 하기 (배경색 변경)
+        currentViewHolder?.itemView?.setBackgroundColor(Color.parseColor("#D3D3D3"))  // 회색으로 배경 변경
+        try {
+            val item = items[position + 1]
+            val regex = Regex("(.*)\\s*\\((.*),(\\d{2}:\\d{2}:\\d{2})\\)") // 타이머가 포함된 형식
+            val matchResult = regex.find(item)
+            val nextViewHolder =
+                adaptorViewList.findViewHolderForAdapterPosition(position + 1) as RecipeDetailRecyclerAdaptor.ViewHolder?
+            // 타이머가 존재하면, 다음 항목의 타이머를 보이게 설정
+            if (matchResult != null) {
+                nextViewHolder?.timerFrame?.visibility = View.VISIBLE
+                val method = matchResult.groupValues[2].trim() // 조리방식 문자열
+                val timeStr = matchResult.groupValues[3].trim() // "hh:mm:ss" 의 시간 비슷한 문자열
+                nextViewHolder?.timerTitle?.text = method
+                nextViewHolder?.timerTime?.text = timeStr
+            } else {
+                // 다음 항목 버튼 바로 보이기
+                nextViewHolder?.doneButton?.visibility = View.VISIBLE
+            }
+        } catch (error: Exception) {
+            Log.e("RecipeCooking", "Error Occured.. :${error.message}") // Index 오류일 것이다. 그대로 둔다.
+        }
+
+    }
 }
+
--- app/src/main/java/com/bcu/foodtable/useful/RecipeDetailRecyclerAdaptor.kt
+++ app/src/main/java/com/bcu/foodtable/useful/RecipeDetailRecyclerAdaptor.kt
@@ -1,20 +1,140 @@
 package com.bcu.foodtable.useful
 
+import android.os.CountDownTimer
 import android.view.*
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.bcu.foodtable.R
 
-class RecipeDetailRecyclerAdaptor(private var items: MutableList<String>): RecyclerView.Adapter<RecipeDetailRecyclerAdaptor.MyViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
-        return MyViewHolder(view)
+class RecipeDetailRecyclerAdaptor(
+    private var items: MutableList<String>,
+    private val onDoneButtonClick: (Int) -> Unit
+) : RecyclerView.Adapter<RecipeDetailRecyclerAdaptor.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemListNumber: TextView = itemView.findViewById(R.id.ItemListNumber) // 순번
+        val itemListName: TextView = itemView.findViewById(R.id.itemListName) // 텍스트
+        val itemTitleNameText : TextView = itemView.findViewById(R.id.itemListTitleName) // 순번 옆 타이틀
+        val checkBox: CheckBox = itemView.findViewById(R.id.checkBox2) // 체크박스
+        val timerFrame: FrameLayout = itemView.findViewById(R.id.ItemTimerFrame) // 타이머 프레임
+        val timerTitle: TextView = itemView.findViewById(R.id.ItemTimerTitle) // 타이머 제목
+        val timerTime: TextView = itemView.findViewById(R.id.ItemTimerTime) // 타이머 시간
+        val timerProgress: ProgressBar = itemView.findViewById(R.id.ItemTimerProgress) // 프로그레스바
+        val startButton: Button = itemView.findViewById(R.id.ItemStartButton) // 타이머 시작 버튼
+        val doneButton: Button = itemView.findViewById(R.id.itemDoneButton) // 완료 버튼
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_recipe_detail_list, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        holder.textView.text = item
+
+        // 순번 설정
+        holder.itemListNumber.text = "${position + 1}."
+        val regexSimple = Regex("^\\d+\\.\\s*\\(([^)]+)\\)\\s*(.*)")
+        val matchResultSimple = regexSimple.find(item)
+        if (matchResultSimple != null) {
+            val category = matchResultSimple.groupValues[1]
+            val content = matchResultSimple.groupValues[2]
+            holder.itemTitleNameText.text=category
+            holder.itemListName.text=content
+        }
+
+
+        // 추가적인 로직 (타이머 등) GONE 상태이기 때문에 이후 타이머 문자열이 포함되어 있으면 VISIBLE로 전환
+        holder.checkBox.isChecked = false // 체크박스 초기화
+
+        // 정규식을 통해 타이머가 존재하는지 확인
+        val regex = Regex("(.*)\\s*\\((.*),(\\d{2}:\\d{2}:\\d{2})\\)")
+        val matchResult = regex.find(item)
+
+
+        if (matchResult != null) {
+            if (position == 0) {
+                holder.timerFrame.visibility = View.VISIBLE
+            }
+            val action = matchResult.groupValues[1].trim() // 앞의 문자열
+            val method = matchResult.groupValues[2].trim() // 조리방식 문자열
+            val timeStr = matchResult.groupValues[3].trim() // "hh:mm:ss" 의 시간 비슷한 문자열
+
+            // 시간 문자열을 초로 변환
+            val totalTimeInSeconds = convertTimeToSeconds(timeStr)
+
+            // 타이머 시작/중지 버튼 클릭 리스너
+            var isTimerRunning = false
+            val timeLeft = totalTimeInSeconds
+
+            if (matchResultSimple != null) {
+                val category = matchResultSimple.groupValues[1]
+                val contentMatch =  Regex("^(.*?)\\s*\\(").find(matchResultSimple.groupValues[2])
+
+                holder.itemTitleNameText.text=category
+                holder.itemListName.text=contentMatch!!.groupValues[1].trim()
+            }
+            // 타이머 아이템 초기화
+            // 텍스트 설정
+            holder.timerTitle.text = method
+            // 프로그레스바 초기화
+            holder.timerProgress.max = totalTimeInSeconds
+            holder.timerProgress.progress = 0
+
+            // 타이머 함수
+            fun startTimer(holder: ViewHolder, timeLeft: Int) {
+                val timer = object : CountDownTimer((timeLeft * 1000).toLong(), 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        val secondsLeft = millisUntilFinished / 1000
+                        holder.timerProgress.progress = (totalTimeInSeconds - secondsLeft).toInt()
+
+                        // 남은 시간 계산
+                        val hours = secondsLeft / 3600
+                        val minutes = (secondsLeft % 3600) / 60
+                        val seconds = secondsLeft % 60
+                        holder.timerTime.text =
+                            String.format("%02d:%02d:%02d", hours, minutes, seconds)
+                    }
+
+                    override fun onFinish() {
+                        holder.timerProgress.progress = totalTimeInSeconds
+                        holder.timerTime.text = "00:00:00"
+                        holder.doneButton.visibility = View.VISIBLE
+                    }
+                }
+
+                // 타이머 시작
+                timer.start()
+            }
+
+            // 타이머 시작 버튼
+            holder.startButton.setOnClickListener {
+                if (!isTimerRunning) {
+                    // 타이머 시작
+                    isTimerRunning = true
+                    startTimer(holder, timeLeft)
+                    holder.startButton.isClickable = false
+                    holder.startButton.visibility = View.GONE
+                }
+            }
+
+        } else {
+            // 타이머가 존재하지 않는 경우
+            holder.timerFrame.visibility = View.GONE
+        }
+        // 타이머 시작 버튼 동작 설정 // 미구현
+
+
+        // 첫 번째 요소에게만 버튼 활성화
+        if (position == 0 && (matchResult == null)) {
+            holder.doneButton.visibility = View.VISIBLE
+        } else {
+            holder.doneButton.visibility = View.GONE
+        }
+
+        holder.doneButton.setOnClickListener() {
+            onDoneButtonClick(position)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -25,10 +145,16 @@ class RecipeDetailRecyclerAdaptor(private var items: MutableList<String>): Recyc
     fun updateItems(newItems: List<String>) {
         items.clear() // 기존 리스트 클리어
         items.addAll(newItems) // 새 아이템 추가
+        items.map { it.replace(Regex("^\\d+\\. "), "") }
         notifyDataSetChanged() // 어댑터에 변경 사항 알리기
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(R.id.textView)
+    // 시간 문자열 (hh:mm:ss)을 초로 변환
+    private fun convertTimeToSeconds(time: String): Int {
+        val timeParts = time.split(":")
+        val hours = timeParts[0].toInt()
+        val minutes = timeParts[1].toInt()
+        val seconds = timeParts[2].toInt()
+        return (hours * 3600) + (minutes * 60) + seconds
     }
 }
