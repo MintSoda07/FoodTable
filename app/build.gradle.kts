e07b65882fe8d4d67832d7107c1c7cee303415c5
MintSoda07 <79971109+MintSoda07@users.noreply.github.com>
2025-04-08 10:14:50 +0900

    오류수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     // 포트원 결제 라이브러리
     implementation("com.github.portone-io:android-sdk:0.1.0")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+
     // 플랙스 레이아웃 사용을 위한 라이브러리
     implementation(libs.flexbox)
 
