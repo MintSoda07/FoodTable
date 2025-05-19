5e5237f453c82fbf13b33d17fa927b87da5bf381
username2015 <l030105@naver.com>
2025-05-19 16:53:23 +0900

    jatpack으로 수정

---
+++ app/src/main/java/com/bcu/foodtable/JetpackCompose/screens/MyChannelScreen.kt
@@ -0,0 +1,21 @@
+package com.bcu.foodtable.JetpackCompose.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyChannelScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "내 채널",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+} 
