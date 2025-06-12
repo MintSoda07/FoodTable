0af684bd734b01f4309edf17fc20883f4b9a148d
username2015 <l030105@naver.com>
2025-06-12 11:31:42 +0900

    친구, 채팅 디자인 수정, 채팅 창에 챗봇 삭제

--- app/src/main/java/com/bcu/foodtable/ui/home/HomeScreen.kt
+++ app/src/main/java/com/bcu/foodtable/ui/home/HomeScreen.kt
@@ -3,8 +3,10 @@ package com.bcu.foodtable.ui.home
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
@@ -140,6 +142,7 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.platform.LocalDensity
 import androidx.navigation.NavType
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.navArgument
 import com.bcu.foodtable.JetpackCompose.Social.DetailedChatScreen
 import com.bcu.foodtable.JetpackCompose.Social.MatzipViewModel
@@ -1162,6 +1165,7 @@ fun AppTopBar(
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
@@ -1174,8 +1178,10 @@ fun HomeScreen(viewModel: HomeViewModel) {
     val loadFailed by viewModel.loadFailed.collectAsState()
     var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
     var selectedCategory by remember { mutableStateOf<String?>(null) }
-
     val navController = rememberNavController()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
     val screens = listOf(
         Screen.Home, Screen.Subscribe, Screen.Social, Screen.RecipeStorage, Screen.MyPage
     )
@@ -1235,10 +1241,12 @@ fun HomeScreen(viewModel: HomeViewModel) {
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = {
-                showBottomSheet = true
-            }) {
-                Icon(Icons.Filled.Chat, contentDescription = "Open AI Chat")
+            if (currentRoute != "chat/{uid}") {
+                FloatingActionButton(onClick = {
+                    showBottomSheet = true
+                }) {
+                    Icon(Icons.Filled.Chat, contentDescription = "Open AI Chat")
+         }
             }
         },
         modifier = Modifier.pointerInput(Unit) {
@@ -1272,7 +1280,9 @@ fun HomeScreen(viewModel: HomeViewModel) {
                 val targetUid = backStackEntry.arguments?.getString("uid") ?: return@composable
                 DetailedChatScreen(
                     navController = navController,
-                    targetUid = targetUid
+                    targetUid = targetUid,
+                    homeViewModel = viewModel
+
                 )
             }
             composable("challenge") {
