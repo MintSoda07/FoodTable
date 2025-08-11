96687de500891c08908c44d5243e364299438a13
MintSoda07 <79971109+MintSoda07@users.noreply.github.com>
2025-08-11 14:52:15 +0900

    채팅 푸시알림

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,28 +1,38 @@
 module.exports = {
+  root: true,
   env: {
     es6: true,
     node: true,
   },
-  parserOptions: {
-    "ecmaVersion": 2018,
-  },
   extends: [
     "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // 빌드 파일 무시
+    "/generated/**/*", // 생성된 파일 무시
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
   ],
   rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+
+    // 🔹 배포 막는 Google style 규칙 완화
+    "require-jsdoc": "off",
+    "max-len": ["error", { code: 120 }],
+    "object-curly-spacing": ["error", "always"],
   },
-  overrides: [
-    {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
-      rules: {},
-    },
-  ],
-  globals: {},
 };
